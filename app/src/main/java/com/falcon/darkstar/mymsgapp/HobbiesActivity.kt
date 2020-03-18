447415bf231cf12cee885dd85a0020b3262e7d71
falcon-ram <62321462+falcon-ram@users.noreply.github.com>
2020-03-18 14:01:31 +0800

    Added connectivity between Recyclerview and data adaptor

--- app/src/main/java/com/falcon/darkstar/mymsgapp/HobbiesActivity.kt
+++ app/src/main/java/com/falcon/darkstar/mymsgapp/HobbiesActivity.kt
@@ -2,10 +2,19 @@ package com.falcon.darkstar.mymsgapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_hobbies.*
 
 class HobbiesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hobbies)
+
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        val adaptor = HobbiesAdaptor(this, Supplier.hobbies)
+        recyclerView.adapter = adaptor  // This recyclerView is from the activities_hobbies.xml
     }
 }
--- app/src/main/java/com/falcon/darkstar/mymsgapp/HobbiesAdaptor.kt
+++ app/src/main/java/com/falcon/darkstar/mymsgapp/HobbiesAdaptor.kt
@@ -1,17 +1,43 @@
 package com.falcon.darkstar.mymsgapp
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list_item.view.*
 
 class HobbiesAdaptor(val context: Context, val hobbies: List<Hobby>) : RecyclerView.Adapter<HobbiesAdaptor.MyViewHolder>() {
 
     inner class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        var currentHobby: Hobby? = null
+        var currentPosition: Int = 0
+
+        init {
+            itemView.setOnClickListener{
+                Toast.makeText(context, currentHobby!!.title + " Clicked! ", Toast.LENGTH_SHORT).show()
+            }
+
+            itemView.imgShare.setOnClickListener {
+                val message : String = "My Hobby is: " + currentHobby!!.title
+
+                val tmpIntent = Intent()
+                tmpIntent.action = Intent.ACTION_SEND
+                tmpIntent.putExtra(Intent.EXTRA_TEXT, message)
+                tmpIntent.type = "text/plain"
+
+                context.startActivity(Intent.createChooser(tmpIntent, "Share to: "))
+            }
+        }
+
         fun setData(hobby: Hobby?, pos : Int) {
             itemView.txvTitle.text = hobby!!.title
+
+            this.currentHobby = hobby
+            this.currentPosition = pos
         }
 
     }
